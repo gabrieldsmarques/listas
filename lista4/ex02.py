0e1a7daa93a99763f8b02733842b192560c6b251
GabrielMarques <ccgkgc@gmail.com>
2025-01-28 21:51:24 -0400

    ex02 foi feito

--- lista4/ex02.py
+++ lista4/ex02.py
@@ -1,7 +1,14 @@
 def soma_lista_aninhada(lista):
-    if type(lista) == list:
-        return soma_lista_aninhada(sum(lista))
-    else:
+    soma = 0
+    if lista == 0:
         return lista
+    else:
+        for i in lista:
+            if isinstance(i, (int, float)):
+                soma += i
+            else:
+                soma += soma_lista_aninhada(i)
+        return soma
+                
 
 print(soma_lista_aninhada([1, [2, 3], [4, [5]]]))
